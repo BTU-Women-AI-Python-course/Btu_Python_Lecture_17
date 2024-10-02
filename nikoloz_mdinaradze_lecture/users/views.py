f71e71b2b24c7038de35f01f1932d62c014501d2
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-02 22:15:51 +0400

    add default authentication

--- nikoloz_mdinaradze_lecture/users/views.py
+++ nikoloz_mdinaradze_lecture/users/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+
 from users.serializers import UserSerializer
 from rest_framework import generics
 
@@ -6,6 +8,11 @@ from rest_framework import generics
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
 
 
 class UserDetails(generics.RetrieveUpdateDestroyAPIView):
