48e901cfabcb46a0f8d5ef9cd2df291d9096148f
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-10-02 20:28:49 +0400

    add blogs model

--- nikoloz_mdinaradze_lecture/blogs/models.py
+++ nikoloz_mdinaradze_lecture/blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=255)
+    context = models.TextField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
